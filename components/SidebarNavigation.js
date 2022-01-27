2bc40a736c65fe8ed605a313ed66999c23ce37d8
Laurent <laurent@demarez.org>
2022-01-27 14:01:24 +0000

    fix: sidebar logo issue

--- components/SidebarNavigation.js
+++ components/SidebarNavigation.js
@@ -69,7 +69,7 @@ const SidebarNavigation = ({showMobile, onClose}) => (
               </div>
             </Transition.Child>
             <div className="flex-shrink-0 flex items-center px-4">
-              <DAOnativeLogo className="mx-auto" />
+              <img src="/DAOnativeLogo.png" width="48" height="48" className="mx-auto" />
             </div>
             <div className="mt-5 flex-1 h-0 overflow-y-auto">
               <nav className="px-2 space-y-1">
@@ -105,7 +105,7 @@ const SidebarNavigation = ({showMobile, onClose}) => (
       {/* Sidebar component, swap this element with another sidebar if you like */}
       <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
         <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-          <DAOnativeLogo className="mx-auto" />
+          <img src="/DAOnativeLogo.png" width="48" height="48" className="mx-auto" />
         </div>
         <div className="flex-1 flex flex-col overflow-y-auto">
           <nav className="flex-1 px-2 py-4 space-y-1">
