89dd837af87a35367318f282ddada1053edce262
Laurent <laurent@demarez.org>
2022-01-28 16:54:28 +0000

    chore: add dapp provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 
+import { ChainId, DAppProvider } from '@usedapp/core'
+import { Toaster } from 'react-hot-toast'
+
+
+
+const config = {
+  readOnlyChainId: ChainId.Polygon,
+  readOnlyUrls: {
+    [ChainId.Polygon]: process.env.NEXT_PUBLIC_POLYGON_RPC,
+  },
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={config}>
+      <Toaster />
+      <Component {...pageProps} />
+    </DAppProvider>
+  )
 }
 
 export default MyApp
