a950ff50da95783f215cd71ccbd48b6151f9bc60
Laurent <laurent@demarez.org>
2022-01-31 13:54:45 +0000

    fix: set title in _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,20 @@ import '../styles/globals.css'
 
 import { UseWalletProvider } from 'use-wallet'
 import { Toaster } from 'react-hot-toast'
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UseWalletProvider chainId={137}>
-      <Toaster />
-      <Component {...pageProps} />
-    </UseWalletProvider>
+    <>
+      <Head>
+        <title>DAOnative</title>
+      </Head>
+      <UseWalletProvider chainId={137}>
+        <Toaster />
+        <Component {...pageProps} />
+      </UseWalletProvider>
+    </>
   )
 }
 
