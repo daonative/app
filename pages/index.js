3d4573dea52c521dd0b8d88e78c4828ca3e404c6
Laurent De Marez <laurent@laurents-mbp.home>
2022-02-04 17:58:56 +0100

    feat: list member DAOs

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,28 @@ import ConnectWalletButton from "../components/ConnectWalletButton"
 import useDarkMode from "../lib/useDarkMode"
 import useIsConnected from "../lib/useIsConnected"
 import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
-
+import Link from "next/link"
 
 const db = getFirestore()
 
-const Home = () => {
+const getRooms = async () => {
+  const db = getFirestore()
+  const snapshot = await getDocs(collection(db, 'rooms'))
+  return snapshot.docs.map((doc) => ({ roomId: doc.id, ...doc.data() }))
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const rooms = await getRooms()
+  return { props: { rooms } }
+}
+
+const Home = ({ rooms }) => {
   const { account } = useWallet()
   const isConnected = useIsConnected()
   const [memberships, loading] = useCollectionDataOnce(
     query(collection(db, 'memberships'), where('account', '==', account || ''))
   )
+  const membershipRooms = memberships?.map(membership => membership.roomId) || []
 
   useDarkMode()
 
@@ -28,8 +40,19 @@ const Home = () => {
     )
   }
 
+  if (loading) {
+    return <>Loading</>
+  }
+
   return (
-    <></>
+    <ul>
+      {rooms
+        .filter(room => membershipRooms.includes(room.roomId))
+        .map(room => (
+          <li key={room.roomId}><Link href={`/dao/${room.roomId}/`}>{room.name}</Link></li>
+        ))
+      }
+    </ul>
   )
 }
 
