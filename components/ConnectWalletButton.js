6fac554470021740cdfb56411ff7bd961d4f7187
Laurent De Marez <laurent@prologe.io>
2022-02-08 12:19:56 +0100

    feat: show PFP in sidebar connect button

--- components/ConnectWalletButton.js
+++ components/ConnectWalletButton.js
@@ -1,15 +1,8 @@
 import { useWallet } from 'use-wallet'
 import ShortAddress from './ShortAddress'
-import { useConnectWalletModal} from './ConnectWalletModal'
+import { useConnectWalletModal } from './ConnectWalletModal'
 import useIsConnected from '../lib/useIsConnected'
-
-const Button = ({ children, onClick }) => (
-  <button
-    className="rounded-lg text-gray-100 bg-daonative-dark-100 hover:bg-daonative-dark-200 hover:text-daonative-gray-200 flex justify-center items-center h-full w-full"
-    onClick={onClick}>
-    {children}
-  </button>
-)
+import PFP from './PFP'
 
 const ConnectWalletButton = () => {
   const wallet = useWallet()
@@ -17,17 +10,23 @@ const ConnectWalletButton = () => {
   const { openConnectWalletModal } = useConnectWalletModal()
 
   return (
-    <>
+    <button
+      className="rounded-lg text-gray-100 bg-daonative-dark-100 hover:bg-daonative-dark-200 hover:text-daonative-gray-200 flex items-center h-full w-full p-4"
+      onClick={() => { !isConnected && openConnectWalletModal() }}
+    >
       {isConnected ? (
-        <Button>
-          <ShortAddress>{wallet.account}</ShortAddress>
-        </Button>
+        <>
+          <div className="px h-8 w-8">
+            <PFP address={wallet.account} size={32} />
+          </div>
+          <div className="px-4">
+            <ShortAddress>{wallet.account}</ShortAddress>
+          </div>
+        </>
       ) : (
-        <Button onClick={openConnectWalletModal}>
-          Connect
-        </Button>
+        <div className="justify-center w-full">Connect</div>
       )}
-    </>
+    </button>
   )
 }
 
