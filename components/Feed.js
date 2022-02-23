b9c05202985369970378e516706048c1e98eb467
Laurent De Marez <laurent@prologe.io>
2022-02-23 12:52:48 +0100

    feat: ability to see work proof in feed

--- components/Feed.js
+++ components/Feed.js
@@ -10,6 +10,7 @@ import PFP from './PFP';
 import ShortAddress from './ShortAddress';
 import Spinner from './Spinner';
 import { EyeOffIcon } from '@heroicons/react/solid';
+import { classNames } from '../lib/utils';
 
 const ReviewModal = ({ show, onClose, event, KPIs }) => {
   const { register, handleSubmit, formState: { isSubmitting, errors } } = useForm()
@@ -89,8 +90,28 @@ const ReviewModal = ({ show, onClose, event, KPIs }) => {
   )
 }
 
+const WorkProofModal = ({ show, onClose, workProof }) => {
+  return (
+    <Modal show={show} onClose={onClose}>
+      <ModalTitle>Work Proof</ModalTitle>
+      <ModalBody>{workProof}</ModalBody>
+      <ModalActionFooter>
+        <div className="flex gap-2">
+          <button
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-daonative-dark-100 dark:text-daonative-gray-100"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </ModalActionFooter>
+    </Modal>
+  )
+}
+
 const Feed = ({ feed, kpis, roomId }) => {
   const [reviewId, setReviewId] = useState()
+  const [workProof, setWorkProof] = useState(null)
   const metrics = mergeKPIsAndDefaults(kpis)
   const { account } = useWallet()
   const membership = useMembership(account, roomId)
@@ -99,103 +120,120 @@ const Feed = ({ feed, kpis, roomId }) => {
   const handleReviewWork = (id) => setReviewId(id)
   const handleCloseReviewModal = () => setReviewId(undefined)
 
+  const handleShowWorkProof = (id) => {
+    const event = feed.find(event => event.eventId === id)
+    setWorkProof(event?.workProof)
+  }
+  const handleCloseWorkProof = () => setWorkProof(null)
+
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 dark:border-daonative-gray-900">
-            <table className="table-fixed min-w-full divide-y divide-gray-200 dark:divide-daonative-gray-900">
-              <thead className="bg-gray-50 dark:bg-daonative-dark-100 text-gray-500 dark:text-daonative-gray-200">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-                  >
-                    Feed
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-                  >
-                    Praise
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-                  >
-                    Author
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-                  >
-                    Date
-                  </th>
-                  {isMember && (
+    <>
+      <WorkProofModal show={!!workProof} onClose={handleCloseWorkProof} workProof={workProof} />
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 dark:border-daonative-gray-900">
+              <table className="table-fixed min-w-full divide-y divide-gray-200 dark:divide-daonative-gray-900">
+                <thead className="bg-gray-50 dark:bg-daonative-dark-100 text-gray-500 dark:text-daonative-gray-200">
+                  <tr>
                     <th
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
                     >
-                      Actions
+                      Feed
                     </th>
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {feed.map((event) => {
-                  const totalPraise = event.praises?.reduce((total, currentPraise) => total + currentPraise.praise, 0)
-                  const impacts = event.praises?.map(praise => praise.impact).filter((v, i, a) => a.indexOf(v) === i) || []
-                  const canReview = (
-                    event.type === "work" &&
-                    event.authorAccount !== account &&
-                    !event.appraisers?.includes(account)
-                  )
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                    >
+                      Praise
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                    >
+                      Author
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                    >
+                      Date
+                    </th>
+                    {isMember && (
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                      >
+                        Actions
+                      </th>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {feed.map((event) => {
+                    const totalPraise = event.praises?.reduce((total, currentPraise) => total + currentPraise.praise, 0)
+                    const impacts = event.praises?.map(praise => praise.impact).filter((v, i, a) => a.indexOf(v) === i) || []
+                    const canReview = (
+                      event.type === "work" &&
+                      event.authorAccount !== account &&
+                      !event.appraisers?.includes(account)
+                    )
 
-                  return (
-                    <tr key={event.eventId} className="bg-white dark:bg-daonative-dark-100 text-gray-900 dark:text-daonative-gray-200">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">{event.description}</td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium flex items-center gap-2">
-                        <div className="flex gap-2">
-                          {impacts.map((impactId) => {
-                            const ImpactIcon = metrics[impactId]?.icon
-                            return ImpactIcon && <ImpactIcon key={`${event.eventId}-${impactId}`} className="h-4 w-4" />
-                          })}
-                          {totalPraise && `+${totalPraise}`}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm">
-                        <div className="flex items-center gap-4">
-                          <PFP address={event.authorAccount} size={40} />
-                          {/* <img className="h-10 w-10 rounded-full" src="https://ipfs.io/ipfs/QmbvBgaAqGVAs3KiEgsuDY2u4BUnuA9ueG96NFSPK4z6b6" alt="" />*/}
-                          {event.authorName ? (
-                            <>{event.authorName}</>
-                          ) : (
-                            <ShortAddress>{event.authorAccount}</ShortAddress>
+                    return (
+                      <tr key={event.eventId} className="bg-white dark:bg-daonative-dark-100 text-gray-900 dark:text-daonative-gray-200">
+                        <td
+                          onClick={() => handleShowWorkProof(event.eventId)}
+                          className={classNames(
+                            "px-6 py-4 whitespace-nowrap text-sm font-medium",
+                            event.workProof && "hover:cursor-pointer"
                           )}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm">
-                        <Moment date={event.created} fromNowDuring={24 * 60 * 60 * 1000} format="yyyy-MM-DD" />
-                      </td>
-                      {isMember && (
-                        <td className="px-6 py-4 whitespace-nowrap text-sm">
-                          {canReview && (
-                            <>
-                              <span className="hover:cursor-pointer underline" onClick={() => handleReviewWork(event.eventId)}>Review</span>
-                              <ReviewModal show={reviewId === event.eventId} onClose={handleCloseReviewModal} event={event} KPIs={kpis} />
-                            </>
-                          )}
+                        >
+                          {event.description}
                         </td>
-                      )}
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </table>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium flex items-center gap-2">
+                          <div className="flex gap-2">
+                            {impacts.map((impactId) => {
+                              const ImpactIcon = metrics[impactId]?.icon
+                              return ImpactIcon && <ImpactIcon key={`${event.eventId}-${impactId}`} className="h-4 w-4" />
+                            })}
+                            {totalPraise && `+${totalPraise}`}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm">
+                          <div className="flex items-center gap-4">
+                            <PFP address={event.authorAccount} size={40} />
+                            {/* <img className="h-10 w-10 rounded-full" src="https://ipfs.io/ipfs/QmbvBgaAqGVAs3KiEgsuDY2u4BUnuA9ueG96NFSPK4z6b6" alt="" />*/}
+                            {event.authorName ? (
+                              <>{event.authorName}</>
+                            ) : (
+                              <ShortAddress>{event.authorAccount}</ShortAddress>
+                            )}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm">
+                          <Moment date={event.created} fromNowDuring={24 * 60 * 60 * 1000} format="yyyy-MM-DD" />
+                        </td>
+                        {isMember && (
+                          <td className="px-6 py-4 whitespace-nowrap text-sm">
+                            {canReview && (
+                              <>
+                                <span className="hover:cursor-pointer underline" onClick={() => handleReviewWork(event.eventId)}>Review</span>
+                                <ReviewModal show={reviewId === event.eventId} onClose={handleCloseReviewModal} event={event} KPIs={kpis} />
+                              </>
+                            )}
+                          </td>
+                        )}
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
