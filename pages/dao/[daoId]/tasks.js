cbdcdee48058d54e8a2f24e77cba968b32344c2c
Laurent De Marez <laurent@prologe.io>
2022-02-23 13:43:55 +0100

    chore: add button to empty task state

--- pages/dao/[daoId]/tasks.js
+++ pages/dao/[daoId]/tasks.js
@@ -62,7 +62,7 @@ const TaskModal = ({ show, onClose, roomId, taskId, defaultValues = {} }) => {
   useEffect(() => {
     // fix timezone offset
     const deadline = defaultValues.deadline && (new Date(defaultValues.deadline - new Date().getTimezoneOffset() * 60000).toISOString()).slice(0, -1)
-    reset({...defaultValues, deadline})
+    reset({ ...defaultValues, deadline })
   }, [reset, defaultValues])
 
   const createTask = async (data) => {
@@ -248,11 +248,21 @@ export default function Tasks({ dao: initialDAO, tasks: initialTasks }) {
               </div>
             </div>
             <div className="mx-auto py-8 px-4 sm:px-6 md:px-8">
-              { tasks?.length > 0 ? (
-              <TasksTable showAssignee={true} showWeight={true} tasks={tasks} onTaskStatusChange={handleTaskStatusChange} onTaskClick={handleEditTask} />
+              {tasks?.length > 0 ? (
+                <TasksTable showAssignee={true} showWeight={true} tasks={tasks} onTaskStatusChange={handleTaskStatusChange} onTaskClick={handleEditTask} />
               ) : (
-                <div className="flex justify-center">
-                  {"Looks like you don't have any tasks."}
+                <div className="flex flex-col gap-2 items-center justify-center">
+                  <div>
+                    <span>{"Looks like you don't have any tasks."}</span>
+                  </div>
+                  <div>
+                    <button
+                      className="mx-2 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-daonative-dark-100 dark:text-daonative-gray-100"
+                      onClick={handleAddTask}
+                    >
+                      Add your first task
+                    </button>
+                  </div>
                 </div>
               )}
             </div>
