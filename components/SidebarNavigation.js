1ecf64fb37ef73ec09e53feccaeca65f4a71f939
Benjamin Shafii <ben@prologe.io>
2022-02-25 09:39:08 +0000

    feat: ability to send feedback

--- components/SidebarNavigation.js
+++ components/SidebarNavigation.js
@@ -1,4 +1,4 @@
-import { Fragment, } from 'react'
+import { Fragment, useState, } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
   CalendarIcon,
@@ -15,9 +15,94 @@ import ComingSoonBadge from './ComingSoonBadge'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import PolygonWarning from './PolygonWarning'
+import { Modal, ModalActionFooter, ModalBody, ModalTitle } from './Modal'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import Spinner from './Spinner'
+import { useWallet } from 'use-wallet'
+import useMembers from '../lib/useMembers'
+import useMembership from '../lib/useMembership'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+const FeedbackModal = ({ show, onClose }) => {
+  const { query: params } = useRouter()
+  const roomId = params?.daoId
+  const { account } = useWallet()
+  const membership = useMembership(account, roomId)
+
+  const { register, handleSubmit, reset, formState: { isSubmitting, errors } } = useForm()
+
+  const submitFeedback = async (data) => {
+    await axios.post('/api/feedback', { content: data.content, user: membership.name })
+  }
+  const handleSubmitFeedback = async (data) => {
+    await submitFeedback(data)
+    onClose()
+    reset()
+  }
+
+
+  return (
+    <Modal show={show} onClose={onClose} >
+      <ModalTitle>Feedback</ModalTitle>
+      <form onSubmit={handleSubmit(handleSubmitFeedback)}>
+        <ModalBody>
+          <textarea rows="8" {...register("content", { required: false })} className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md dark:bg-daonative-dark-100 dark:border-transparent dark:text-daonative-gray-300" />
+        </ModalBody>
+        <ModalActionFooter>
+          <button
+            type="submit"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-daonative-dark-100 dark:text-daonative-gray-100"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <span className="w-4 h-4 mx-auto"><Spinner /></span>
+            ) : (
+              <>Submit Feedback</>
+            )}
+          </button>
+
+        </ModalActionFooter>
+      </form>
+    </Modal>)
+}
+
+const Feedback = () => {
+  const { query: params } = useRouter()
+  const roomId = params?.daoId
+  const { account } = useWallet()
+  const membership = useMembership(account, roomId)
+  const isMember = !!membership
+
+
+  const [showModal, setShowModal] = useState(false)
+  const handleShowModal = () => {
+    setShowModal((showModal) => !showModal)
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+
+
+  if (!isMember) return <></>
+
+  return (
+    <>
+      <FeedbackModal show={showModal} onClose={handleClose} />
+      <button
+        onClick={handleShowModal}
+        type="button"
+        className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Give Feedback on DAOnative
+      </button>
+    </>
+  )
 }
 
 const SidebarNavigation = ({ showMobile, onClose }) => {
@@ -116,6 +201,7 @@ const SidebarNavigation = ({ showMobile, onClose }) => {
                   })}
                 </nav>
                 <div className="py-5 px-4 flex flex-col gap-2">
+                  <Feedback />
                   <PolygonWarning />
                   <div className="h-14">
                     <ConnectWalletButton />
@@ -171,6 +257,7 @@ const SidebarNavigation = ({ showMobile, onClose }) => {
               })}
             </nav>
             <div className="py-5 px-4 flex flex-col gap-2">
+              <Feedback />
               <PolygonWarning />
               <div className="h-14">
                 <ConnectWalletButton />
---
+++ pages/api/feedback.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+
+const webhookURL = process.env.DAONATIVE_DISCORD_WEBHOOK
+
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        // Process a POST request
+        const content = req.body?.content
+        const user = req.body?.user
+        const webhookBody = {
+            "username": `${user} via Feedback Bot`,
+            "avatar_url": "https://www.daonative.xyz/frame-logo.png",
+            "content": content,
+        }
+        axios.post(webhookURL, webhookBody)
+        res.status(200).send({ content: content })
+    } else {
+        return {}
+        // Handle any other HTTP method
+    }
+}
