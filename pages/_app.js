3affdf9c95ae80eecffadad6e4db68ca187d8a6f
Benjamin Shafii <ben@prologe.io>
2022-02-25 08:48:36 +0000

    chore: fix simple analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 import { UseWalletProvider } from 'use-wallet'
 import { Toaster } from 'react-hot-toast'
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps } from 'firebase/app'
 import { getAnalytics } from "firebase/analytics";
 import Head from 'next/head';
 import { ConnectWalletModalProvider } from '../components/ConnectWalletModal';
@@ -11,11 +11,15 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: "1:208459706334:web:1138476679353f3ba5b74f",
   measurementId: "G-BJ2Y4Y62SY"
 }
 
-initializeApp(firebaseConfig)
-getAnalytics(app);
+if (!getApps().length) {
+  const app = initializeApp(firebaseConfig)
+  getAnalytics(app);
+}
+
 
 function MyApp({ Component, pageProps }) {
   return (
