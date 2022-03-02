0beea68c8486de6ede3c971130a2e13d212762a7
Laurent <laurent@demarez.org>
2022-03-02 11:27:35 +0000

    fix: add a LayoutWrapper component

---
+++ components/LayoutWrapper.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import HeaderNavigation from './HeaderNavigation';
+import SidebarNavigation from './SidebarNavigation';
+
+export const LayoutWrapper = ({ children }) => {
+  const [showSidebarMobile, setShowSidebarMobile] = useState(false);
+
+  const handleShowMobileSidebar = () => setShowSidebarMobile(true);
+  const handleCloseMobileSidebar = () => setShowSidebarMobile(false);
+
+  // Temp until everyting is dark
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  });
+
+  return (
+    <div>
+      <SidebarNavigation showMobile={showSidebarMobile} onClose={handleCloseMobileSidebar} />
+      <HeaderNavigation onShowSidebar={handleShowMobileSidebar} onToggleDarkMode={() => { }} showLogWork={false} />
+      <div className="md:pl-64 flex-row md:flex overflow-hidden dark:bg-daonative-dark-300 dark:text-daonative-gray-100">
+        <main className="w-full py-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
