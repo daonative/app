e2c9ebf1e6b30948099442a8e31082354ca625e3
Laurent <laurent@demarez.org>
2022-03-04 13:33:18 +0000

    chore: leaderboard design + mock data

--- pages/dao/[daoId]/leaderboard.js
+++ pages/dao/[daoId]/leaderboard.js
@@ -1,46 +1,81 @@
-import { CheckIcon } from "@heroicons/react/solid"
+import { CheckIcon, UsersIcon } from "@heroicons/react/solid"
 import Link from "next/link"
 import { PrimaryButton } from "../../../components/Button"
 import { LayoutWrapper } from "../../../components/LayoutWrapper"
+import PFP from "../../../components/PFP"
 import useMembers from "../../../lib/useMembers"
 
-const challenges = []
+const kFormatter = (num) =>
+    Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
 
 const Leaderboard = () => {
-  const members = useMembers()
+  const leaders = [
+    {
+      userAccount: 'odsijflsdfjsdlkfj',
+      userName: 'zordan',
+      userPFP: 'test',
+      experience: 7900,
+      submissionCount: 204,
+      position: 1
+    },
+    {
+      userAccount: 'dlfkjsdlfkj',
+      userName: 'ben',
+      userPFP: 'test',
+      experience: 3500,
+      submissionCount: 204,
+      position: 2
+    },
+    {
+      userAccount: 'jldskfjsldfkjdslj',
+      userName: 'rose8',
+      userPFP: 'test',
+      experience: 2500,
+      submissionCount: 204,
+      position: 3
+    },
+    {
+      userAccount: 'foidjsfldskj',
+      userName: 'lrnt',
+      userPFP: 'test',
+      experience: 1000,
+      submissionCount: 204,
+      position: 4
+    },
+  ]
 
   return (
     <LayoutWrapper>
       <div className="mx-auto px-4 sm:px-6 md:px-8">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between">
-            <h2 className="text-2xl">Challenges</h2>
+            <h2 className="text-2xl">Leaderboard</h2>
             <PrimaryButton>Claim reward</PrimaryButton>
           </div>
           <ul role="list" className="flex flex-col gap-3">
-            {members?.map((member) => (
-              <Link key={member.challengeId} href={`/dao/${roomId}/challenges/${member.challengeId}`}>
-                <li>
-                  <div className="px-4 py-4 sm:px-6 bg-daonative-dark-100 rounded hover:cursor-pointer hover:bg-daonative-dark-200">
-                    <div className="flex items-center justify-between">
-                      <p className="text-sm font-medium text-daonative-gray-100 truncate">{member.title}</p>
-                      <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
-                        {member.weight} XP
-                      </span>
-                    </div>
-                    <div className="mt-2 sm:flex sm:justify-between">
-                      <div className="sm:flex">
-                      </div>
-                      <div className="mt-2 flex items-center text-sm text-daonative-gray-300 sm:mt-0">
-                        <CheckIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-daonative-primary-blue" />
-                        <p>
-                          {member?.meta?.submissionCount || 0} Completions
-                        </p>
-                      </div>
+            {leaders?.map((leader, idx) => (
+              <li key={idx}>
+                <div className="px-4 py-4 sm:px-6 bg-daonative-dark-100 rounded flex justify-between">
+                  <div className="flex items-center gap-3">
+                    <span className="px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 text-gray-800 font-weight-600 font-space">
+                      #{String(leader.position).padStart(3, '0')}
+                    </span>
+                    <PFP address={leader.userAccount} size={38} />
+                    <p className="text-sm font-medium text-daonative-gray-100">{leader.userName}</p>
+                  </div>
+                  <div className="mt-2 sm:flex flex-col items-end gap-0.5">
+                    <span className="px-2.5 py-0.5 rounded-md text-xs font-medium bg-blue-100 text-blue-800 font-weight-600 font-space">
+                      {kFormatter(leader.experience)} XPs
+                    </span>
+                    <div className="mt-2 flex items-center text-sm text-daonative-gray-300 sm:mt-0">
+                      <CheckIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-daonative-primary-blue" />
+                      <p>
+                        {leader.submissionCount || 0} Completions
+                      </p>
                     </div>
                   </div>
-                </li>
-              </Link>
+                </div>
+              </li>
             ))}
           </ul>
         </div>
