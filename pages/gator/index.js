002e03e82dcb7d3b8ac0401259a100cdb3f83dc1
Laurent <laurent@demarez.org>
2022-03-09 19:58:11 +0000

    feat: ui cleanup

--- pages/gator/index.js
+++ pages/gator/index.js
@@ -13,6 +13,7 @@ import useProvider from "../../lib/useProvider"
 import toast from "react-hot-toast"
 import Link from "next/link"
 import axios from "axios"
+import { Modal, ModalActionFooter, ModalBody, ModalTitle } from "../../components/Modal"
 
 const COLLECION_CREATOR_CONTRACT = "0x01a2fdf22abdd94c909048a345ee26e5425452ab"
 
@@ -38,7 +39,7 @@ export const Header = ({ children }) => {
   )
 }
 
-const CollectionForm = () => {
+const CreateCollectionModal = ({ show, onClose }) => {
   const { account, chainId } = useWallet()
   const provider = useProvider()
   const { register, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm()
@@ -52,7 +53,6 @@ const CollectionForm = () => {
       formData,
       { headers: { "Content-Type": "multipart/form-data" } }
     )
-    console.log(response)
     return `https://ipfs.infura.io/ipfs/${response.data.Hash}`
   }
 
@@ -79,56 +79,94 @@ const CollectionForm = () => {
     toast.success("NFT collection created", { id: toastId })
     reset()
   }
-
   return (
-    <>
-      <form onSubmit={handleSubmit(handleCreateCollection)}>
-        <div className="flex flex-col gap-4">
-          <div>
-            <label className="block text-sm font-medium pb-2">
-              Collection Name
-            </label>
-            <input type="text" rows="8" {...register("name", { required: true })} placeholder="School DAO Membership" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300" />
-            {errors.name && (
-              <span className="text-xs text-red-400">You need to set a name</span>
-            )}
+    <Modal show={show} onClose={onClose}>
+      <ModalTitle>Create your collection</ModalTitle>
+      {isPolygon && (
+        <form onSubmit={handleSubmit(handleCreateCollection)}>
+          <ModalBody>
+            <div className="flex flex-col gap-4">
+              <div>
+                <label className="block text-sm font-medium pb-2">
+                  Collection Name
+                </label>
+                <input type="text" rows="8" {...register("name", { required: true })} placeholder="School DAO Membership" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300" />
+                {errors.name && (
+                  <span className="text-xs text-red-400">You need to set a name</span>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium pb-2">
+                  Symbol
+                </label>
+                <input type="text" rows="8" {...register("symbol", { required: true })} placeholder="SDM" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300" />
+                {errors.symbol && (
+                  <span className="text-xs text-red-400">You need to set a symbol</span>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm font-medium pb-2">
+                  Image
+                </label>
+                <input {...register("image", { required: true })} type="file" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-100 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300 pr-4" />
+                {errors.image && (
+                  <span className="text-xs text-red-400">You need to set an image</span>
+                )}
+              </div>
+              <div>
+              </div>
+            </div>
+          </ModalBody>
+          <ModalActionFooter>
+            <PrimaryButton type="sumbit">Create collection</PrimaryButton>
+          </ModalActionFooter>
+        </form>
+      )}
+      {!isPolygon && (
+        <ModalBody>
+          <div className="p-12">
+            <PolygonWarning />
           </div>
-          <div>
-            <label className="block text-sm font-medium pb-2">
-              Symbol
-            </label>
-            <input type="text" rows="8" {...register("symbol", { required: true })} placeholder="SDM" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300" />
-            {errors.symbol && (
-              <span className="text-xs text-red-400">You need to set a symbol</span>
-            )}
-          </div>
-          <div>
-            <label className="block text-sm font-medium pb-2">
-              Image
-            </label>
-            <input {...register("image", { required: true })} type="file" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-100 rounded-md bg-daonative-dark-100 border-transparent text-daonative-gray-300 pr-4" />
-            {errors.image && (
-              <span className="text-xs text-red-400">You need to set an image</span>
-            )}
-          </div>
-          <div>
-            {isPolygon && <PrimaryButton type="sumbit">Create collection</PrimaryButton>}
-          </div>
-        </div>
-      </form>
-      <PolygonWarning />
-    </>
+        </ModalBody>
+      )}
+    </Modal>
+  )
+}
+
+const CollectionList = ({ collections }) => {
+  return (
+    <ul role="list" className="flex flex-col gap-3">
+      {
+        collections?.map(collection => (
+          <li key={collection.address}>
+            <Link href={`/gator/${collection.address}`}>
+              <a>
+                <div className="px-4 py-4 sm:px-6 bg-daonative-dark-100 rounded flex gap-4 justify-between">
+                  <p className="text-sm font-medium text-daonative-gray-100">{collection.name}</p>
+                  <span className="px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 text-gray-800 font-weight-600 font-space">
+                    {collection.symbol}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          </li>
+        ))}
+    </ul>
   )
 }
 
 const Gator = () => {
   const { account } = useWallet()
+  const [showCreateModal, setShowCreateModal] = useState(false)
   const [collections, setCollections] = useState([])
   const [collectionsLoading, setCollectionsLoading] = useState(true)
   const provider = new providers.JsonRpcProvider(
     process.env.NEXT_PUBLIC_RPC_POLYGON
   )
   const myCollections = collections.filter(collection => collection.owner === account)
+
+  const handleShowCreateModal = () => setShowCreateModal(true)
+  const handleCloseCreateModal = () => setShowCreateModal(false)
 
   const getCollectionName = (address) => {
     const contract = new ethers.Contract(address, collectionAbi, provider)
@@ -140,15 +178,28 @@ const Gator = () => {
     return contract.owner()
   }
 
+  const getSymbol = (address) => {
+    const contract = new ethers.Contract(address, collectionAbi, provider)
+    return contract.symbol()
+  }
+
   useInterval(async () => {
     const contract = new ethers.Contract(COLLECION_CREATOR_CONTRACT, collectionCreatorAbi, provider)
     const collectionAddresses = await contract.getCollections()
     const collections = await Promise.all(
-      collectionAddresses.map(async address => ({
-        address,
-        name: await getCollectionName(address),
-        owner: await getOwner(address)
-      }))
+      collectionAddresses.map(async address => {
+        const [name, owner, symbol] = await Promise.all([
+          getCollectionName(address),
+          getOwner(address),
+          getSymbol(address)
+        ])
+        return {
+          address,
+          name,
+          owner,
+          symbol
+        }
+      })
     )
     setCollections(collections)
     setCollectionsLoading(false)
@@ -156,27 +207,21 @@ const Gator = () => {
 
   return (
     <div>
+      <CreateCollectionModal show={showCreateModal} onClose={handleCloseCreateModal} />
       <Header>
         Gator
       </Header>
-      <div className="flex flex-col md:flex-row gap-8 p-8">
-        <div className="md:w-96">
-          <CollectionForm />
-        </div>
-        <div>
-          <ul>
+      <div className="flex justify-center">
+        <div className="flex flex-col gap-8 p-8 w-full lg:w-3/4">
+          <div className="flex justify-between items-center w-full">
+            <h2>My NFT Collections</h2>
+            <PrimaryButton onClick={handleShowCreateModal}>Create Collection</PrimaryButton>
+          </div>
+          <div className="w-full">
             {collectionsLoading && "loading..."}
             {!collectionsLoading && myCollections.length === 0 && "No NFT collections found"}
-            {myCollections.map(collection => (
-              <li key={collection.address}>
-                <Link href={`/gator/${collection.address}`}>
-                  <a>
-                    {collection.name} <span className="text-xs">({collection.address})</span>
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+            <CollectionList collections={myCollections} />
+          </div>
         </div>
       </div>
     </div >
