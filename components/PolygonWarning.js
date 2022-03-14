8aa11300dab811e6a559d22b41639c4e4423b4a7
Laurent <laurent@demarez.org>
2022-03-14 17:16:42 +0000

    feat: add a button to switch to mainnet

--- components/PolygonWarning.js
+++ components/PolygonWarning.js
@@ -1,8 +1,9 @@
 import { useWallet } from "use-wallet"
 import PolygonLogo from "../public/PolygonLogo.svg"
+import EthereumLogo from "../public/EthereumLogo.svg"
 
 const PolygonWarning = () => {
-  const { chainId, account, ethereum } = useWallet()
+  const { ethereum } = useWallet()
 
   const handleSwitchToPolygon = async () => {
     const params = [{
@@ -19,14 +20,19 @@ const PolygonWarning = () => {
     await ethereum.request({ method: 'wallet_addEthereumChain', params })
   }
 
-  if (!account) return <></>
-  if (chainId === 137) return <></>
+  const handleSwitchToMainnet = async () => {
+    await ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '0x1' }] })
+  }
 
   return (
-    <div>
-      <div className="text-xs w-full text-center pb-1">
+    <div className="flex flex-col gap-2">
+      <div className="text-xs w-full text-center">
         {"You're using an unsupported network."}
       </div>
+      <button className="flex items-center justify-center gap-2 rounded-lg text-gray-100 bg-daonative-dark-100 p-2 w-full hover:bg-daonative-dark-300 hover:text-daonative-gray-200 " onClick={handleSwitchToMainnet}>
+        <EthereumLogo className="h-4 w-4" />
+        Switch to Ethereum mainnet
+      </button>
       <button className="flex items-center justify-center gap-2 rounded-lg text-gray-100 bg-daonative-dark-100 p-2 w-full hover:bg-daonative-dark-300 hover:text-daonative-gray-200 " onClick={handleSwitchToPolygon}>
         <PolygonLogo className="h-4 w-4" />
         Switch to Polygon
