22312418991eebbb85b4ed76048042a582b8a01b
Benjamin Shafii <ben@prologe.io>
2022-03-17 21:20:35 +0000

    feat: add copy button to invite modal

--- components/InviteMemberModal.js
+++ components/InviteMemberModal.js
@@ -1,58 +1,43 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from '@headlessui/react';
+import { PrimaryButton, SecondaryButton } from "./Button";
+import { Modal, ModalActionFooter, ModalBody, ModalTitle } from "./Modal";
+import toast from "react-hot-toast";
+import { ClipboardCopyIcon } from "@heroicons/react/solid";
 
-export const InviteMemberModal = ({ open, onClose, inviteLink }) => (
-  <Transition.Root show={open} as={Fragment}>
-    <Dialog as="div" className="fixed z-40 inset-0 overflow-y-auto" onClose={onClose}>
-      <div className="flex justify-center items-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+export const InviteMemberModal = ({ open, onClose, inviteLink }) => {
+  const handleCopyToClipboard = async () => {
+    await navigator.clipboard.writeText(inviteLink)
+    toast.success('Copied invite link to clipboard', { icon: <ClipboardCopyIcon className="h-6 w-6 text-green" /> })
+  }
 
-        {/* This element is to trick the browser into centering the modal contents. */}
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-          &#8203;
-        </span>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          enterTo="opacity-100 translate-y-0 sm:scale-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        >
-          <div className="inline-block align-bottom  bg-daonative-dark-300 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-md sm:w-full">
-            <div>
-              <h3 className="text-xl p-4">Invite</h3>
-            </div>
-            <div className="p-4">
-              <p className="text-sm pb-4">Share this link to allow people to join your DAO</p>
-              <input
-                type="text"
-                value={inviteLink}
-                className="w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-component-bg border-transparent text-daonative-gray-300"
-                disabled />
-            </div>
-            <div className="flex justify-end px-4 pb-4 pt-6">
-              <button
-                className="place-self-center inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={onClose}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </Transition.Child>
-      </div>
-    </Dialog>
-  </Transition.Root>
-);
+  return (
+    <Modal show={open} onClose={onClose}>
+      <ModalTitle>Invite</ModalTitle>
+      <ModalBody>
+
+        <p className="text-sm pb-4">Share this link to allow people to join your DAO</p>
+        <input
+          type="text"
+          value={inviteLink}
+          className="w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md bg-daonative-component-bg border-transparent text-daonative-gray-300"
+          disabled />
+
+      </ModalBody>
+      <ModalActionFooter>
+        <div className="flex justify-between w-full">
+          <SecondaryButton
+            onClick={onClose}
+          >
+            Close
+          </SecondaryButton>
+          <PrimaryButton onClick={handleCopyToClipboard} >
+            <ClipboardCopyIcon className="h-4 w-4 mr-1" />
+            Copy to clipboard
+          </PrimaryButton>
+        </div>
+      </ModalActionFooter>
+
+    </Modal >
+  )
+}
