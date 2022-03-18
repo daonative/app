79f6fcceca097478ba38978bf5b32df11f3cceb8
Benjamin Shafii <ben@prologe.io>
2022-03-18 13:59:39 +0000

    style: invert opacity change on card element

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 export const Card = ({ children }) => {
   return (
-    <div className="px-4 py-4 sm:px-6 bg-daonative-component-bg rounded-lg drop-shadow-daonative hover:cursor-pointer hover:opacity-75">
+    <div className="px-4 py-4 sm:px-6 bg-daonative-component-bg rounded-lg drop-shadow-daonative hover:cursor-pointer opacity-[85%] hover:opacity-100">
       {children}
     </div>
   );
