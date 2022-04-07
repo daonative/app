cc8a366b67bd9647969b9f10197ade163a19838d
Benjamin Shafii <ben@prologe.io>
2022-04-07 08:30:53 +0000

    feat: update no dao to link to /onboarding

--- components/EmptyStateNoDAOs.js
+++ components/EmptyStateNoDAOs.js
@@ -1,13 +1,14 @@
 import { UserGroupIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 import { PrimaryButton } from "./Button";
 
 const EmptyStateNoDAOs = () => (
   <div className="w-full p-8 text-center flex flex-col items-center">
-    <UserGroupIcon className="h-24 w-24 m-8" />
-    <h3 className="mt-2 text-lg font-medium text-daonative-gray-100">{"You're not part of any DAO"}</h3>
-    <a href="https://daonative.xyz" target="_blank" rel="noreferrer" >
-      <PrimaryButton>Request Access</PrimaryButton>
-    </a>
+    <UserGroupIcon className="h-24 w-24 m-8 text-daonative-text" />
+    <h3 className="mt-2 text-lg font-medium text-daonative-white">{"You're not part of any DAO"}</h3>
+    <Link passHref href="/onboarding">
+      <PrimaryButton className="mt-3">Create your DAO</PrimaryButton>
+    </Link>
   </div>
 )
 
