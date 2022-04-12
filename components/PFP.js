e899ab4ef56c3de4b5b2bf0bc07dfc270a6f112d
Laurent <laurent@demarez.org>
2022-04-12 07:27:55 +0000

    fix: use jazzicons for broken ens avatars

--- components/PFP.js
+++ components/PFP.js
@@ -1,4 +1,5 @@
 import { CheckCircleIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 import useUserProfile from '../lib/useUserProfile'
 
@@ -9,25 +10,28 @@ const PFP = ({ address, size }) => (
 
 export const UserRectangleAvatar = ({ account, size = 80 }) => {
   const { ensAvatar } = useUserProfile(account, false)
+  const [ ensAvatarError, setEnsAvatarError] = useState(false)
 
-  if (!ensAvatar)
-    return <>
+  const handleEnsAvatarError = () => setEnsAvatarError(true)
 
-      <PFP address={account} size={size} />
-    </>
+  if (!ensAvatar || ensAvatarError)
+    return <PFP address={account} size={size} />
 
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src={ensAvatar} alt="Member avatar" className={`rounded-lg h-[80px] w-[80px]`} />
+  return <img src={ensAvatar} alt="Member avatar" className={`rounded-lg h-[80px] w-[80px]`} onError={handleEnsAvatarError} />
 }
 
 export const UserAvatar = ({ account, size = 40 }) => {
   const { ensAvatar } = useUserProfile(account, false)
+  const [ ensAvatarError, setEnsAvatarError] = useState(false)
 
-  if (!ensAvatar)
+  const handleEnsAvatarError = () => setEnsAvatarError(true)
+
+  if (!ensAvatar || ensAvatarError)
     return <PFP address={account} size={size} />
 
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src={ensAvatar} alt="Member avatar" className={`rounded-full h-${size / 4} w-${size / 4}`} />
+  return <img src={ensAvatar} alt="Member avatar" className={`rounded-full h-${size / 4} w-${size / 4}`} onError={handleEnsAvatarError} />
 }
 
 export const UserName = ({ account }) => {
