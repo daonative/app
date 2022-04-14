e27d2054797752ddff903c4ae2ccb1f3e41c527c
Benjamin Shafii <ben@prologe.io>
2022-04-14 17:38:53 +0000

    style(card): replace card bg

--- components/Card.js
+++ components/Card.js
@@ -23,13 +23,10 @@ export const SimpleCardBody = ({ children, className }) => {
 
 export const Card = ({ onClick, children, className }) => {
   return (
-    <div onClick={onClick} className={classNames(
-      "px-4 py-4 sm:px-6 bg-daonative-component-bg rounded-lg",
-      "hover:cursor-pointer opacity-[85%] hover:opacity-100",
-      "drop-shadow-daonative",
-      className
-    )}>
-      {children}
-    </div>
+    <SimpleCard onClick={onClick} className={classNames(className, onClick && 'opacity-75 hover:opacity-100')}>
+      <SimpleCardBody>
+        {children}
+      </SimpleCardBody>
+    </SimpleCard>
   );
 };
