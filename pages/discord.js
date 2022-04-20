96a880fe46d58b30579c07c66008e7fd0cb5a4e0
Laurent <laurent@demarez.org>
2022-04-20 17:31:04 +0000

    feat: add discord connect page

---
+++ pages/discord.js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const DiscordConnect = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+
+    const connectDiscordGuild = async () => {
+      const fragment = new URLSearchParams(window.location.hash.slice(1))
+      const accessToken = fragment.get('access_token')
+      const tokenType = fragment.get('token_type')
+      const error = fragment.get('error')
+
+      if (error) {
+        window.opener.setDiscordError(error)
+        window.close()
+        return
+      }
+
+      if (!accessToken || !tokenType) {
+        window.opener.setDiscordError("no_token")
+        window.close()
+        return
+      }
+
+      window.opener.setDiscordToken({accessToken, tokenType})
+      window.close()
+    }
+    connectDiscordGuild()
+  }, [])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DiscordConnect
