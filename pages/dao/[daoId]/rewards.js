d9dee91b39f9ef1096f956169f20489586fb048a
Laurent <laurent@demarez.org>
2022-05-10 10:00:50 +0000

    feat: reward details + delete

--- pages/dao/[daoId]/rewards.js
+++ pages/dao/[daoId]/rewards.js
@@ -7,16 +7,16 @@ import useRoomId from "@/lib/useRoomId"
 import { CheckIcon, PlusIcon } from "@heroicons/react/solid"
 import Image from "next/image"
 import { useWallet } from "@/lib/useWallet"
-import { PrimaryButton } from "@/components/Button"
+import { PrimaryButton, SecondaryButton } from "@/components/Button"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { useRequireAuthentication } from "@/lib/authenticate"
-import { addDoc, collection, getFirestore, query, where } from "firebase/firestore"
-import { useCollectionData } from "react-firebase-hooks/firestore"
+import { addDoc, collection, deleteDoc, doc, getFirestore, query, where } from "firebase/firestore"
+import { useCollection, useCollectionData } from "react-firebase-hooks/firestore"
 import { classNames } from "@/lib/utils"
 import EmptyStateNoRewards from "@/components/EmptyStateNoRewards"
 
-const RewardModal = ({ show, onClose }) => {
+const CreateRewardModal = ({ show, onClose }) => {
   const { handleSubmit, register, formState: { errors } } = useForm()
   const requireAuthentication = useRequireAuthentication()
   const roomId = useRoomId()
@@ -108,38 +108,119 @@ const RewardModal = ({ show, onClose }) => {
   )
 }
 
-const RewardItem = ({ title, type, weight, eligibleCount }) =>
-  <Card onClick={() => null}>
-    <div className="flex justify-between">
-      <div className="flex gap-3">
-        <div>
-          <Image width="64" height="64" src="/reward.svg" alt="sample-challenge-picture" />
+const RewardDetailModal = ({ show, onClose, reward }) => {
+  const requireAuthentication = useRequireAuthentication()
+
+  const deleteReward = async () => {
+    const db = getFirestore()
+    await deleteDoc(doc(db, 'rewards', reward.rewardId))
+  }
+
+  const handleDeleteReward = async () => {
+    await requireAuthentication()
+    await deleteReward()
+  }
+
+  const capitalizeFirstLetter = (string) => {
+    try {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    } catch (e) {
+      return string
+    }
+  }
+
+  const title = reward.name
+  const weight = reward?.conditions?.minXps
+  const eligibleCount = reward?.meta?.eligibleCount
+  const role = capitalizeFirstLetter(reward?.reward?.role)
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <ModalTitle>{title}</ModalTitle>
+      <ModalBody>
+        <div className="flex flex-col gap-2">
+          <div>
+            <p className="block text-sm font-medium pb-0.5 text-daonative-subtitle">
+              Reward:
+            </p>
+            <div className="text-sm font-medium text-daonative-white">
+              {role} role
+            </div>
+          </div>
+          <div>
+            <p className="block text-sm font-medium pb-0.5 text-daonative-subtitle">
+              Requirement:
+            </p>
+            <div className="text-sm font-medium text-daonative-white">
+              at least {weight} XP
+            </div>
+          </div>
+          <div>
+            <p className="block text-sm font-medium pb-0.5 text-daonative-subtitle">
+              Eligible members:
+            </p>
+            <div className="text-sm font-medium text-daonative-white">
+              {eligibleCount}
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-3">
-          <p className="text-sm font-semibold  whitespace-normal ">{title}</p>
-          <div className="text-sm text-daonative-subtitle">
-            {type}
+      </ModalBody>
+      <ModalActionFooter>
+        <div className="flex flex-col items-end gap-1">
+          <SecondaryButton onClick={handleDeleteReward}>Delete</SecondaryButton>
+          <span className="text-xs text-daonative-subtitle">💡 Deleting this will not remove the rewards from people who were already eligible</span>
+        </div>
+      </ModalActionFooter>
+    </Modal >
+  )
+}
+
+const RewardItem = ({ reward }) => {
+  const [detailModalOpen, setDetailModalOpen] = useState(false)
+
+  const handleOpenDetailModal = () => setDetailModalOpen(true)
+  const handleCloseDetailModal = () => setDetailModalOpen(false)
+
+  const title = reward.name
+  const type = "Role"
+  const weight = reward?.conditions?.minXps
+  const eligibleCount = reward?.meta?.eligibleCount
+
+  return (
+    <Card onClick={handleOpenDetailModal}>
+      <RewardDetailModal show={detailModalOpen} onClose={handleCloseDetailModal} reward={reward} />
+      <div className="flex justify-between">
+        <div className="flex gap-3">
+          <div>
+            <Image width="64" height="64" src="/reward.svg" alt="sample-challenge-picture" />
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="text-sm font-semibold  whitespace-normal ">{title}</p>
+            <div className="text-sm text-daonative-subtitle">
+              {type}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-end min-w-max gap-3">
+          <div className={classNames(
+            "flex items-center text-sm text-daonative-gray-300",
+            !eligibleCount && "invisible"
+          )}>
+            <CheckIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-daonative-primary-blue" />
+            <p>
+              {eligibleCount} Members
+            </p>
+          </div>
+          <div>
+            <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+              {weight} XPs
+            </span>
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-end min-w-max gap-3">
-        <div className={classNames(
-          "flex items-center text-sm text-daonative-gray-300",
-          !eligibleCount && "invisible"
-        )}>
-          <CheckIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-daonative-primary-blue" />
-          <p>
-            {eligibleCount} Members
-          </p>
-        </div>
-        <div>
-          <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
-            {weight} XPs
-          </span>
-        </div>
-      </div>
-    </div>
-  </Card >
+    </Card>
+  )
+}
 
 const Rewards = () => {
   const db = getFirestore()
@@ -148,14 +229,16 @@ const Rewards = () => {
   const membership = useMembership(account, roomId)
   const isAdmin = membership?.roles?.includes('admin')
   const [rewardModalOpen, setRewardModalOpen] = useState(false)
-  const [rewards, loading] = useCollectionData(query(collection(db, 'rewards'), where('roomId', '==', roomId || '')))
+  const [rewardsSnap, loading] = useCollection(query(collection(db, 'rewards'), where('roomId', '==', roomId || '')))
+
+  const rewards = rewardsSnap?.docs.map(doc => ({ rewardId: doc.id, ...doc.data() })) || []
 
   const handleShowRewardModal = () => setRewardModalOpen(true)
   const handleCloseRewardModal = () => setRewardModalOpen(false)
 
   return (
     <LayoutWrapper>
-      <RewardModal show={rewardModalOpen} onClose={handleCloseRewardModal} />
+      <CreateRewardModal show={rewardModalOpen} onClose={handleCloseRewardModal} />
       <div className="mx-auto px-4 sm:px-0">
         <div className="flex flex-col gap-4">
           <div className="flex justify-between">
@@ -164,15 +247,8 @@ const Rewards = () => {
           </div>
           {rewards?.length > 0 && (
             <ul role="list" className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-              {rewards?.map((reward, index) => <RewardItem
-                key={index}
-                title={reward.name}
-                type="Role"
-                weight={reward?.conditions?.minXps}
-                eligibleCount={reward?.meta?.eligibleCount}
-              />)}
+              {rewards?.map((reward, index) => <RewardItem key={index} reward={reward} />)}
             </ul>
-
           )}
 
           {!loading && rewards?.length === 0 && (
@@ -194,7 +270,7 @@ const Rewards = () => {
           )}
         </div>
       </div>
-    </LayoutWrapper >
+    </LayoutWrapper>
   )
 }
 
