a6cfa4b051d8a456d48c4c6a47dd145791012479
Laurent <laurent@demarez.org>
2022-05-12 11:46:16 +0000

    feat: submit work not tied to a challenge

---
+++ pages/dao/[daoId]/work.js
@@ -0,0 +1,168 @@
+import { PrimaryButton } from "@/components/Button"
+import { SimpleCard } from "@/components/Card"
+import EmptyStateNoSubmissions from "@/components/EmptyStateNoSubmissions"
+import { FileInput } from "@/components/Input"
+import { LayoutWrapper } from "@/components/LayoutWrapper"
+import { Modal, ModalActionFooter, ModalBody, ModalTitle } from "@/components/Modal"
+import { UserName, UserRectangleAvatar } from "@/components/PFP"
+import Spinner from "@/components/Spinner"
+import { TextArea } from "@/components/TextArea"
+import { useRequireAuthentication } from "@/lib/authenticate"
+import { uploadToIPFS } from "@/lib/uploadToIPFS"
+import { useWallet } from "@/lib/useWallet"
+import { BanIcon, CheckIcon, ClockIcon, PlusIcon } from "@heroicons/react/solid"
+import { addDoc, collection, getFirestore, orderBy, query, serverTimestamp, where } from "firebase/firestore"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { useCollection } from "react-firebase-hooks/firestore"
+import { useForm } from "react-hook-form"
+import Moment from "react-moment"
+
+const SubmitProofOfWorkModal = ({ show, onClose, roomId }) => {
+  const { register, handleSubmit, reset, formState: { errors, isSubmitting } } = useForm()
+  const requireAuthentication = useRequireAuthentication()
+  const { account } = useWallet()
+
+  const submitProof = async (description, image) => {
+    const imageUrl = image?.length === 1 ? await uploadToIPFS(image[0]) : ''
+    const proof = {
+      description,
+      author: account,
+      roomId,
+      type: 'adhoc',
+      imageUrls: imageUrl ? [imageUrl] : [],
+      created: serverTimestamp(),
+    }
+    const db = getFirestore()
+    await addDoc(collection(db, 'workproofs'), proof)
+  }
+
+  const handleSubmitProof = async (data) => {
+    await requireAuthentication()
+    await submitProof(data.description, data.image)
+    onClose()
+    reset()
+  }
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <form onSubmit={handleSubmit(handleSubmitProof)}>
+        <ModalTitle>Proof of Work</ModalTitle>
+        <ModalBody>
+          <div className="flex flex-col gap-4">
+            <div>
+              <TextArea label="Description" name="description" required register={register} />
+              {errors.description && (
+                <span className="text-xs text-red-400">You need to set a description</span>
+              )}
+            </div>
+            <FileInput name="image" register={register} />
+          </div>
+        </ModalBody>
+        <ModalActionFooter>
+          <PrimaryButton type="submit">
+            {isSubmitting ? (
+              <span className="w-4 h-4 mx-auto"><Spinner /></span>
+            ) : (
+              <>Submit Proof of Work</>
+            )}
+          </PrimaryButton>
+        </ModalActionFooter>
+      </form>
+    </Modal >
+  )
+}
+
+const WorkList = ({ submissions }) => {
+  if (submissions?.length === 0) return <EmptyStateNoSubmissions />
+
+  return (
+    <>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {submissions?.map((submission, idx) => {
+          const verifications = submission?.verifications ? Object.values(submission.verifications) : []
+          const isPending = verifications.length === 0
+          const isReverted = !isPending && verifications.filter(verification => !verification.accepted).length > 0
+          const isVerified = !isPending && !isReverted
+          return (
+            <li key={idx} >
+              <SimpleCard className="hover:cursor-pointer opacity-[75%] hover:opacity-100">
+                <div className=' py-2 px-3'>
+                  <div className="grid grid-cols-2 overflow-hidden">
+                    <UserRectangleAvatar account={submission.author} />
+                    <div className="flex flex-col gap-1 items-end ">
+                      <p className="text-sm  max-w-[100%] truncate text-ellipsis">
+                        <UserName account={submission.author} />
+                      </p>
+                      <p className="text-sm text-gray-500 ">
+                        <Moment date={submission?.created?.toMillis()} fromNow={true} />
+                      </p>
+                      <div className="flex justify-between">
+                        {isVerified && (
+                          <div className="inline-flex gap-1 items-center">
+                            <CheckIcon className="w-5 h-5 text-daonative-primary-blue" />
+                            <p className="text-sm">Verified</p>
+                          </div>
+                        )}
+                        {isPending && (
+                          <div className="inline-flex gap-1 items-center text-daonative-white">
+                            <ClockIcon className="w-5 h-5" />
+                            <p className="text-sm">Pending</p>
+                          </div>
+                        )}
+                        {isReverted && (
+                          <div className="inline-flex gap-1 items-center text-daonative-white">
+                            <BanIcon className="w-5 h-5" />
+                            <p className="text-sm">Reverted</p>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SimpleCard>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+const WorkPage = () => {
+  const [showProofModal, setShowProofModal] = useState(false)
+  const { query: params } = useRouter()
+  const roomId = params.daoId || ''
+
+  const handleOpenProofModal = () => setShowProofModal(true)
+  const handleCloseProofModal = () => setShowProofModal(false)
+
+  const db = getFirestore()
+  const [workproofsSnapshot] = useCollection(
+    query(collection(db, 'workproofs'), where('roomId', '==', roomId), orderBy('created', 'desc'))
+  )
+  const workproofs = workproofsSnapshot?.docs.map(doc => ({ ...doc.data(), workproofId: doc.id }))
+
+  return (
+    <LayoutWrapper>
+      <SubmitProofOfWorkModal show={showProofModal} onClose={handleCloseProofModal} roomId={roomId} />
+      <div className="mx-auto px-4 sm:px-6 md:px-8">
+        <div className="w-full">
+          <div className="flex justify-between py-4">
+            <div>
+              <h2 className="text-xl text-daonative-white">Submissions</h2>
+            </div>
+            <div className="flex gap-4 items-center">
+              <button className="bg-daonative-primary-blue flex justify-center items-center rounded-full h-8 w-8 p-0" onClick={handleOpenProofModal}>
+                <PlusIcon className="w-4 h-4" />
+              </button>
+            </div>
+          </div>
+          <WorkList workproofs={workproofs} />
+        </div>
+      </div>
+    </LayoutWrapper >
+  )
+}
+
+export default WorkPage
