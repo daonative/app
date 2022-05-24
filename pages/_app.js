d0352e14d9e14b2ae07c51247a3df03c15f3884d
Benjamin Shafii <ben@prologe.io>
2022-05-24 10:05:09 +0000

    chore: use correct import for wagmi provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,82 +1,82 @@
-import '../styles/globals.css'
-import '@rainbow-me/rainbowkit/styles.css';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-import { Toaster } from 'react-hot-toast'
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import Head from 'next/head';
-import { ConnectWalletModalProvider } from '../components/ConnectWalletModal';
-import { useEffect, useMemo } from 'react'
-import { ProfileModalProvider } from '../components/ProfileModal'
-import { WagmiProvider, createClient, chain } from 'wagmi'
+import { Toaster } from "react-hot-toast";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import Head from "next/head";
+import { ConnectWalletModalProvider } from "../components/ConnectWalletModal";
+import { useEffect, useMemo } from "react";
+import { ProfileModalProvider } from "../components/ProfileModal";
+import { WagmiConfig, createClient, chain } from "wagmi";
 
 import {
   apiProvider,
   configureChains,
   getDefaultWallets,
   RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { connectAuthEmulator, getAuth } from 'firebase/auth';
-import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+} from "@rainbow-me/rainbowkit";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-}
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
-initializeApp(firebaseConfig)
+initializeApp(firebaseConfig);
 
-const EMULATORS_STARTED = 'EMULATORS_STARTED';
+const EMULATORS_STARTED = "EMULATORS_STARTED";
 function startEmulators() {
-  if (global[EMULATORS_STARTED]) return
+  if (global[EMULATORS_STARTED]) return;
 
   const auth = getAuth();
   const db = getFirestore();
   connectAuthEmulator(auth, "http://localhost:9099");
-  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectFirestoreEmulator(db, "localhost", 8080);
   global[EMULATORS_STARTED] = true;
 }
 
 if (process.env.NEXT_PUBLIC_EMULATOR === "true") {
-  startEmulators()
+  startEmulators();
 }
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.rinkeby, chain.polygon, chain.optimism, chain.arbitrum],
-  [
-    apiProvider.infura(process.env.NEXT_PUBLIC_INFURA_API_KEY),
-    apiProvider.fallback()
-  ]
+  [apiProvider.infura(process.env.NEXT_PUBLIC_INFURA_API_KEY), apiProvider.fallback()],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'DAOnative',
-  chains
+  appName: "DAOnative",
+  chains,
 });
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if (typeof window === "undefined")
-      return
+    if (typeof window === "undefined") return;
 
-    getAnalytics()
-  }, [])
+    getAnalytics();
+  }, []);
 
-  const wagmiClient = useMemo(() => createClient({
-    autoConnect: true,
-    connectors,
-    provider
-  }), [])
+  const wagmiClient = useMemo(
+    () =>
+      createClient({
+        autoConnect: true,
+        connectors,
+        provider,
+      }),
+    [],
+  );
 
   return (
     <>
       <Head>
         <title>DAOnative</title>
       </Head>
-      <WagmiProvider client={wagmiClient}>
+      <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
           <ConnectWalletModalProvider>
             <ProfileModalProvider>
@@ -85,9 +85,9 @@ function MyApp({ Component, pageProps }) {
             </ProfileModalProvider>
           </ConnectWalletModalProvider>
         </RainbowKitProvider>
-      </WagmiProvider>
+      </WagmiConfig>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
