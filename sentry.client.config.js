5f4ed73a837f14a49da6bf0c6c98fad984e89db1
Benjamin Shafii <ben@prologe.io>
2022-05-24 10:30:35 +0000

    chore(sentry): capture console.errors

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,16 +2,22 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
+import { CaptureConsole } from "@sentry/integrations";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 if (env !== "development") {
   // do something
   Sentry.init({
-    dsn: SENTRY_DSN || 'https://4ef32e42ad524d09a7d423e52762284e@o1209338.ingest.sentry.io/6343156',
+    dsn: SENTRY_DSN || "https://4ef32e42ad524d09a7d423e52762284e@o1209338.ingest.sentry.io/6343156",
     // Adjust this value in production, or use tracesSampler for greater control
+    integrations: [
+      new CaptureConsole({
+        levels: ["error"],
+      }),
+    ],
     tracesSampleRate: 1.0,
     // ...
     // Note: if you want to override the automatic release value, do not set a
